8dce554c70cf649a8a4d1c8ce663d9dee5eb1faf
Cindy Do <cindydo@Cindys-MBP.fios-router.home>
2020-11-03 00:30:52 -0800

    building cmake and make files

---
+++ CMakeFiles/unit-test.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/cindydo/lab-04---composite-pattern-cdo032-galva041-lab4/unit_test.cpp" "/Users/cindydo/lab-04---composite-pattern-cdo032-galva041-lab4/CMakeFiles/unit-test.dir/unit_test.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
