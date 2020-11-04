11b80047c8de6da4ce9037b9b559bc1a454ae132
Cindy Do <cindydo@Cindys-MBP.fios-router.home>
2020-11-03 21:05:01 -0800

    added more tests

--- op_test.hpp
+++ op_test.hpp
@@ -6,6 +6,9 @@
 #include "op.hpp"
 #include "Mult.hpp"
 #include "Div.hpp"
+#include "add.hpp"
+#include "sub.hpp"
+#include "pow.hpp"
 
 TEST(OpTest, OpEvaluateNonZero) {
     Op* test = new Op(8);
@@ -20,6 +23,21 @@ TEST(MultTest, MultEvaluateNonZero) {
 TEST(DivTest, DivEvaluateNonZero) {
     Div* test = new Div(6,2);
     EXPECT_EQ(test->evaluate(), 3);
+}
+
+TEST(AddTest, AddEvalNonZero) {
+    Add* test = new Add(3,7);
+    EXPECT_EQ(test->evaluate(), 10);
+}
+
+TEST(SubTest, AddEvalNonZero) {
+    Sub* test = new Sub(9,3);
+    EXPECT_EQ(test->evaluate(), 6);
+}
+
+TEST(PowTest, AddEvalNonZero) {
+    Pow* test = new Pow(5,2);
+    EXPECT_EQ(test->evaluate(), 25);
 }
 
 #endif //__OP_TEST_HPP__
