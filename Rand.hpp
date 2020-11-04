c6f125fd6c16ade0c39b824d5daf58b036b6086b
Gabriela Alvarez <gaby@Gabrielas-MacBook-Pro.local>
2020-11-03 20:22:24 -0800

    Rand.hpp defined

---
+++ Rand.hpp
@@ -0,0 +1,16 @@
+#ifndef __RAND_HPP__
+#define __RAND_HPP__
+
+#include "base.hpp"
+#include <string>
+
+class Rand : public Base {
+    private: 
+        double val = rand()%100;
+    public:
+        Rand() : Base() {}
+        virtual double evaluate() { return val; }
+        virtual std::string stringify() { return std::to_string(val); }
+};
+
+#endif //__OP_HPP__
