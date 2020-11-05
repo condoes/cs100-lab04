efada418531ac223de190980b1787c13d9390e8d
Cindy <cindy.do10@gmail.com>
2020-11-04 23:06:54 -0800

    modified add_test.hpp

--- add_test.hpp
+++ add_test.hpp
@@ -6,26 +6,26 @@
 #include "add.hpp"
 
 TEST(AddTest, AddEvalNonZero) {
-    Op* three = new Op(3);
-    Op* seven = new Op(7);
+    Base* three = new Op(3);
+    Base* seven = new Op(7);
     Add* test = new Add(three,seven);
     EXPECT_EQ(test->evaluate(), 10);
     EXPECT_EQ(test->stringify(), "3.000000 + 7.000000");
 }
 
 TEST(AddTest, AddEvalZero) {
-    Op* zero = new Op(0);
+    Base* zero = new Op(0);
     Add* test = new Add(zero,zero);
     EXPECT_EQ(test->evaluate(), 0);
     EXPECT_EQ(test->stringify(), "0.000000 + 0.000000");
 }
 
 TEST(AddTest, AddEvalNegative) {
-    Op* two = new Op(2);
-    Op* negFive = new Op(-5);
+    Base* two = new Op(2);
+    Base* negFive = new Op(-5);
     Add* test = new Add(two,negFive);
     EXPECT_EQ(test->evaluate(), -3);
-    EXPECT_EQ(test->stringify(), "2.000000 + -3.000000");
+    EXPECT_EQ(test->stringify(), "2.000000 + -5.000000");
 }
 
 
