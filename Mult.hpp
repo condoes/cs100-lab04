4b2535c1d87cf553c7229190c1b38abd736330f4
Gabriela Alvarez <galva041@ucr.edu>
2020-11-04 23:18:19 -0800

    Mult.hpp updated

--- Mult.hpp
+++ Mult.hpp
@@ -2,20 +2,21 @@
 #define __MULT_HPP__
 
 #include "base.hpp"
+#include "op.hpp"
 #include <string>
 
 class Mult : public Base {
     private: 
-        double v1, v2, result;
+        Base* v1;
+        Base* v2;
     public:
-        Mult(double val1, double val2) : Base() { 
+        Mult(Base* val1, Base* val2) : Base() { 
             v1 = val1; 
             v2 = val2;
-            result = v1 * v2;
         }
-        virtual double evaluate() { return result; }
-        virtual std::string stringify() { 
-            std::string myString = std::to_string(v1) + " * " + std::to_string(v2);
+        double evaluate() { return v1->evaluate() * v2->evaluate(); }
+        std::string stringify() { 
+            std::string myString = v1->stringify() + " * " + v2->stringify();
             return myString; 
         }
 };
