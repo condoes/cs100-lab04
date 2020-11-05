6f3b56ad8bc3701645c262d9c1fa8bea70dabedc
Cindy Do <cindydo@Cindys-MBP.fios-router.home>
2020-11-04 17:15:41 -0800

    fixed pow.hpp parameters

--- pow.hpp
+++ pow.hpp
@@ -2,21 +2,22 @@
 #define __POW_HPP__
 
 #include "base.hpp"
+#include "op.hpp"
 #include <string>
 #include <cmath>
 
 class Pow : public Base {
     private: 
-        double base; 
-        double exponent;
+        Op* base; 
+        Op* exponent;
     public:
-        Pow(double base, double exp) : Base() { 
+        Pow(Op* base, Op* exp) : Base() { 
             this->base = base;
             exponent = exp; 
         }
-        virtual double evaluate() { return pow(base, exponent); }
+        virtual double evaluate() { return pow(base->evaluate(), exponent->evaluate()); }
         virtual std::string stringify() { 
-            std::string str = std::to_string(base) + " ** " + std::to_string(exponent);
+            std::string str = base->stringify() + " ** " + exponent->stringify();
             return str;
         }
 };
